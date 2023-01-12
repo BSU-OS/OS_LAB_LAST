6ef2154eaf283903fa7f05d4ba0878f731ea6c41
Falkramm <matus.daniil@mail.ru>
2023-01-12 22:36:12 +0300

    add Message Queue

---
+++ MessageQueue/B/main.py
@@ -0,0 +1,30 @@
+import time
+
+import win32com.client
+import os
+
+def write():
+    qinfo=win32com.client.Dispatch("MSMQ.MSMQQueueInfo")
+    computer_name = os.getenv('COMPUTERNAME')
+    qinfo.FormatName="direct=os:"+computer_name+"\\PRIVATE$\\InputQueue"
+    queue=qinfo.Open(2,0)   # Open a ref to queue
+    msg=win32com.client.Dispatch("MSMQ.MSMQMessage")
+    msg.Label="Client"
+    msg.Body = "Hi from client"
+    msg.Send(queue)
+    queue.Close()
+def read():
+    qinfo = win32com.client.Dispatch("MSMQ.MSMQQueueInfo")
+    computer_name = os.getenv('COMPUTERNAME')
+    qinfo.FormatName = "direct=os:" + computer_name + "\\PRIVATE$\\OutputQueue"
+    queue = qinfo.Open(1, 0)  # Open a ref to queue to read(1)
+    msg = queue.Receive()
+    print("Label:", msg.Label)
+    print("Body :", msg.Body)
+    # msg=win32com.client.Dispatch("MSMQ.MSMQMessage")
+    # msg.Label="Client"
+    # msg.Body = "Hi from client"
+    # msg.Send(queue)
+    queue.Close()
+read()
+write()
